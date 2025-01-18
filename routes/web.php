5df81e9f88a153246a6516dec36c95d3ffdf0ed2
Waseem Altinawi <wasemm@live.com>
2025-01-18 18:31:43 +0100

    adding property type

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Backend\PropertyTypeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -27,17 +28,21 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function(){
 
-    Route::get('/admin/dashboard', [AdminController::class, 'adminDashboard'])->name('admin.dashboard');
+    Route::controller(AdminController::class)->group(function(){
+        Route::get('/admin/dashboard',  'adminDashboard')->name('admin.dashboard');
 
-    Route::get('/admin/logout', [AdminController::class, 'adminLogout'])->name('admin.logout');
+        Route::get('/admin/logout',  'adminLogout')->name('admin.logout');
+    
+        Route::get('/admin/profile',  'adminProfile')->name('admin.profile');
+    
+        Route::post('/admin/profile/store',  'adminProfileStore')->name('admin.profile.store');
+    
+        Route::get('/admin/change/password',  'adminChangePassword')->name('admin.change.password');
+    
+        Route::post('/admin/update/password',  'adminUpdatePassword')->name('admin.update.password');
+    });
 
-    Route::get('/admin/profile', [AdminController::class, 'adminProfile'])->name('admin.profile');
-
-    Route::post('/admin/profile/store', [AdminController::class, 'adminProfileStore'])->name('admin.profile.store');
-
-    Route::get('/admin/change/password', [AdminController::class, 'adminChangePassword'])->name('admin.change.password');
-
-    Route::post('/admin/update/password', [AdminController::class, 'adminUpdatePassword'])->name('admin.update.password');
+    
 });
 
 // End Admin Group Middleware
@@ -53,3 +58,17 @@ Route::get('/user/dashboard', [UserController::class, 'userDashboard'])->name('u
 
 
 Route::get('/admin/login', [AdminController::class, 'adminLogin'])->name('admin.login');
+
+
+// Admin Group Middleware
+Route::middleware(['auth', 'role:admin'])->group(function(){
+
+//Property Type All Route
+    Route::controller(PropertyTypeController::class)->group(function(){
+
+        Route::get('/all/type', 'allType')->name('all.type');
+
+    });
+
+    
+});
